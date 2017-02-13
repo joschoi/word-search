7459d986ba09dffc6bac2711d731e8fd01fd45b1
joschoi <joschoi@expedia.com>
2017-02-13 13:33:24 -0800

    Basic Outline

---
+++ app/src/main/java/com/personal/joschoi/wordsearch/WordSearchActivity.java
@@ -0,0 +1,13 @@
+package com.personal.joschoi.wordsearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WordSearchActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_word_search);
+	}
+}
---
+++ app/src/main/java/com/personal/joschoi/wordsearch/WordSearchViewModel.java
@@ -0,0 +1,8 @@
+package com.personal.joschoi.wordsearch;
+
+/**
+ * Created by joschoi on 2/13/17.
+ */
+
+public class WordSearchViewModel {
+}
